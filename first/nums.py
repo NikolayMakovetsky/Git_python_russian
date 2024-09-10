46c49458281fc9ea646f5088cd9b8aca36ad65ef
NikolayMakovetsky <nicholasmakovetsky@gmail.com>
2024-09-10 15:12:42 +0300

    rename func plus() in nums.py / get final merge

--- first/nums.py
+++ first/nums.py
@@ -1,6 +1,6 @@
 
 
-def plus(a, b, c):
+def plusfuncresult(a, b, c):
     return a + b + c
 
 
@@ -12,6 +12,6 @@ def mul(a, b):
     return a*b
 
 if __name__ == '__main__':
-    print(plus(2, 2, 4))
+    print(plusfuncresult(2, 2, 4))
     print(minus(5, 2))
     print(mul(3, 3))
